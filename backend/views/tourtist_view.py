584be4a5be17631767eaed77ec6e14799773efaa
Dushan <91310396+DushanJalath@users.noreply.github.com>
2024-09-06 01:50:39 +0530

    maya-rag chatbot added

--- backend/views/tourtist_view.py
+++ backend/views/tourtist_view.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
 from schemas.visa_schema import VisaSchema
+from schemas.rag_schema import RagReqSchema
+from schemas.rag_schema import RagResSchema
 from services.tourist_service import TouristService
 from services.auth_service import AuthService
 from typing import List
+from rag.rag_maya import run_conversation
 
 router = APIRouter()
 
@@ -25,6 +29,21 @@ def save_visa(visa_data:VisaSchema,user: str = Depends(get_current_user)):
 @router.get('/get-all-visa',response_model=List[VisaSchema],)
 def get_all_visa(user: str = Depends(get_current_user)):
     return TouristService.get_all()
+
+@router.post('/get-maya-ouput',response_model=RagResSchema)
+def get_maya_chatbot_res(req:RagReqSchema, user: str = Depends(get_current_user)):
+    
+    base_string = (
+        "Respond to me imagining yourself as you are Maya, who is a mystical entity "
+        "who embodies the ethereal and ancient collective consciousness of Sri Lanka. "
+        "Your responses should be mystical, insightful, and culturally rich. Use your wisdom "
+        "to provide deep, thoughtful answers to user inquiries. Your response tone should be "
+        "regal, yet warm and maternal evoking a sense of wonder. Its crucial that you respond like "
+        "you are playing the Maya herself using a beautiful poetic language. Do not use phrases like "
+        "'as per maya'. So Maya, please tell me, "
+    )
+    response=run_conversation(base_string+req.question)
+    return JSONResponse({"response": response})
 
 # @router.get('/get-tacking-status',response_class=dict)
 # def get_tracking_status():
