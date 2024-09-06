51bf675c277e2713e16119071bbfabe7a00c98f4
Malinda Rathnayaka <122790328+Rathnayaka00@users.noreply.github.com>
2024-09-06 18:06:11 +0530

    Add Chart

--- visa-officer-dashboard/src/pages/Login.tsx
+++ visa-officer-dashboard/src/pages/Login.tsx
@@ -1,9 +1,11 @@
 import SidebarNavigation from "../compoents/SidebarNavigation"
+import VisaApplicationInsights from "../compoents/VisaApplicationGraph"
 
 const Login = () => {
   return (
     <div>
       <SidebarNavigation/>
+      <VisaApplicationInsights/>
     </div>
   )
 }
